e4eda996d5b42eae7cd976524c6980dd966a1658
blueju <blueju@foxmail.com>
2021-07-27 12:49:09 +0800

    update src\MyBreadcrumb\index.tsx，直接暴露引入的组件

--- src/MyBreadcrumb/index.tsx
+++ src/MyBreadcrumb/index.tsx
@@ -1,16 +1,3 @@
-import React from 'react';
 import { Breadcrumb } from 'antd';
 
-function MyBreadcrumb(props: any) {
-  return (
-    <Breadcrumb {...props} separator=">">
-      {props.children}
-    </Breadcrumb>
-  );
-}
-
-const { Item, Separator } = Breadcrumb;
-MyBreadcrumb.Item = Item;
-MyBreadcrumb.Separator = Separator;
-
-export default MyBreadcrumb;
+export default Breadcrumb;
