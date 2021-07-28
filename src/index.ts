ef0a3ebe68ecfb92436c1ff6cae5e64dd99f4503
blueju <blueju@foxmail.com>
2021-07-28 18:44:39 +0800

    add src\MyConfigProvider\index.tsx

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { default as MyButton } from './MyButton';
 export { default as MyPagination } from './MyPagination';
 export { default as MyBreadcrumb } from './MyBreadcrumb';
 export { default as MyAnchor } from './MyAnchor';
+export { default as MyConfigProvider } from './MyConfigProvider';
