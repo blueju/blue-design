8f14f6623982f413f49f9d9dfda1e2e1c8edbad9
blueju <blueju@foxmail.com>
2021-07-28 14:13:03 +0800

    update src\MyAnchor\index.tsx

--- src/MyAnchor/index.tsx
+++ src/MyAnchor/index.tsx
@@ -1,4 +1,50 @@
 import React from 'react';
 import { Anchor } from 'antd';
 
-export default Anchor;
+// console.dir(Anchor);
+
+// 继承方式的高阶组件
+// function HOC(WrappedComponent: any) {
+//   return class HOC extends WrappedComponent {
+//     constructor(props: any) {
+//       super(props);
+//     }
+//     render() {
+//       return super.render();
+//     }
+//   };
+// }
+// export default HOC(Anchor);
+
+// 代理方式的高阶组件（推荐）
+// function HOC(WrappedComponent: any) {
+//   class MyAnchor extends React.Component {
+//     static Link: any;
+//     constructor(props: any) {
+//       super(props);
+//     }
+//     render() {
+//       return (
+//         <WrappedComponent {...this.props}>
+//           {this.props.children}
+//         </WrappedComponent>
+//       );
+//     }
+//   }
+//   MyAnchor.Link = WrappedComponent.Link;
+//   return MyAnchor;
+// }
+// export default HOC(Anchor);
+
+// 一开始的方式
+class MyAnchor extends React.Component {
+  static Link: any;
+  constructor(props: any) {
+    super(props);
+  }
+  render() {
+    return <Anchor {...this.props}>{this.props.children}</Anchor>;
+  }
+}
+MyAnchor.Link = Anchor.Link;
+export default MyAnchor;
