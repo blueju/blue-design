58a4911c5c358783dd0af1e9b20b5cc5f1f5c3e7
blueju <blueju@foxmail.com>
2021-07-28 22:37:24 +0800

    open mfsu

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   // more config: https://d.umijs.org/config
-  // mfsu: {},
+  /**+
+   * 修改菜单结构时需要关闭，
+   * 详见：https://github.com/umijs/dumi/issues/795
+   */
+  mfsu: {},
   mode: 'site',
 });
