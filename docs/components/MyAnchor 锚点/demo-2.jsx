498d5ea6e96a4aba3a3f40234d08761d2167c11b
blueju <blueju@foxmail.com>
2021-08-13 18:46:59 +0800

    update anchor demo

--- docs/components/MyAnchor 锚点/demo-2.jsx
+++ docs/components/MyAnchor 锚点/demo-2.jsx
@@ -4,9 +4,12 @@ import { MyAnchor } from 'blued';
 export default () => {
   return (
     <MyAnchor bounds={10} anchorType="line">
-      <MyAnchor.Link href="#components-anchor-demo-basic" title="Basic demo" />
       <MyAnchor.Link
-        href="#components-anchor-demo-static"
+        href="#/components/my-anchor 锚点#components-anchor-demo-basic"
+        title="Basic demo"
+      />
+      <MyAnchor.Link
+        href="#/components/my-anchor 锚点#components-anchor-demo-static"
         title="Static demo"
       />
     </MyAnchor>
