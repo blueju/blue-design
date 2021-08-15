86200cb455acfcf1532672ee7a4da3a0bad3eda6
blueju <blueju@foxmail.com>
2021-08-15 17:09:27 +0800

    add upload demo

---
+++ docs/components/MyUpload 上传/demo-1.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MyUpload, MyButton } from 'blued';
+
+export default () => {
+  return (
+    <MyUpload>
+      <MyButton>Click to Upload</MyButton>
+    </MyUpload>
+  );
+};
