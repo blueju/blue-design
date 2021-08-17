9124f234c652ea96f527be170e3b1649e318cad6
blueju <blueju@foxmail.com>
2021-08-17 20:54:35 +0800

    update upload components

--- src/MyUpload/index.tsx
+++ src/MyUpload/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Progress, Upload } from 'antd';
+import { Progress, Upload, Row, Col } from 'antd';
+import { UploadFile } from 'antd/lib/upload/interface';
+import {
+  BorderOutlined,
+  CloseCircleFilled,
+  CheckCircleFilled,
+  LoadingOutlined,
+} from '@ant-design/icons';
 import styles from './index.less';
 import IconFont from '../iconfont';
 
@@ -12,14 +19,67 @@ class MyUpload extends React.Component {
     this.itemRender = this.itemRender.bind(this);
   }
 
+  getStatusIcon(file: UploadFile) {
+    switch (file.status) {
+      case 'uploading':
+        return <LoadingOutlined className={styles.loadingOutlined} />;
+      case 'error':
+        return file.percent === 100 ? (
+          <CheckCircleFilled className={styles.checkCircleFilled} />
+        ) : (
+          <CloseCircleFilled className={styles.closeCircleFilled} />
+        );
+      case 'done':
+        return <CheckCircleFilled className={styles.checkCircleFilled} />;
+    }
+  }
+
+  getPercentOrReUploadIcon(file: UploadFile) {
+    if (file.status === 'error') {
+      return <IconFont type="icon-re-upload" className={styles.reUploadIcon} />;
+    } else {
+      return (
+        <div className={styles.percent}>
+          {Math.floor(file.percent || 0) + '%'}
+        </div>
+      );
+    }
+  }
+
   // @ts-ignore
   itemRender(originNode, file, fileList, actions) {
     console.log(originNode, file, fileList, actions);
     return (
-      <>
-        {originNode}
-        <Progress></Progress>
-      </>
+      <div style={{ padding: 6 }}>
+        <Row gutter={6}>
+          <Col flex="none">
+            <IconFont type="icon-word" style={{ fontSize: 18 }} />
+          </Col>
+          <Col flex="auto">{file.name}</Col>
+          <Col flex="none">
+            <CloseCircleFilled
+              style={{ fontSize: 16, color: '#d8d8d8' }}
+              onClick={actions.remove}
+            />
+          </Col>
+        </Row>
+        <Row gutter={6} align="middle">
+          <Col flex="none">
+            <div style={{ width: 18, fontSize: 10, textAlign: 'right' }}>
+              {this.getStatusIcon(file)}
+            </div>
+          </Col>
+          <Col flex="auto">
+            <Progress
+              strokeWidth={4}
+              showInfo={false}
+              status="active"
+              percent={file.percent}
+            />
+          </Col>
+          <Col flex="none">{this.getPercentOrReUploadIcon(file)}</Col>
+        </Row>
+      </div>
     );
   }
 
--- src/iconfont.ts
+++ src/iconfont.ts
@@ -1,7 +1,7 @@
 import { createFromIconfontCN } from '@ant-design/icons';
 
 const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_2747331_y1aotrdwoa8.js',
+  scriptUrl: '//at.alicdn.com/t/font_2747331_jpsgbn0855c.js',
 });
 
 export default IconFont;
