e8c4c8727d7c158ac974be5f44f68a5fb9a3a9b0
blueju <blueju@foxmail.com>
2022-05-02 15:17:24 +0800

    解决了如何打 umd 包及 umd 包无法使用的问题 https://www.yuque.com/blueju/blog/xvf2gk

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -4,5 +4,12 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   esm: 'rollup',
   cjs: 'rollup',
+  umd: {
+    globals: {
+      blued: 'blued',
+    },
+    name: 'blued',
+    file: 'blued',
+  },
   extraRollupPlugins: [commonjs()],
 };
